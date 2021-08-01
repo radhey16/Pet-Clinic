af98e8351a63a949140231d97ae9d6609ecd8fca
Radhey Patel <62771845+radhey16@users.noreply.github.com>
2021-08-01 17:49:38 -0400

    Update AddAppointments.js

--- src/components/AddAppointments.js
+++ src/components/AddAppointments.js
@@ -1,175 +1,111 @@
-import React, { Component } from 'react';
-import { FaPlus } from 'react-icons/fa';
+import { BiCalendarPlus } from "react-icons/bi";
+import { useState } from 'react';
 
-class AddAppointments extends Component {
-  constructor() {
-    super();
-    this.state = {
-      petName: '',
-      ownerName: '',
-      aptDate: '',
-      aptTime: '',
-      aptNotes: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleAdd = this.handleAdd.bind(this);
+const AddAppointment = ({ onSendAppointment, lastId }) => {
+  const clearData = {
+    ownerName: '',
+    petName: '',
+    aptDate: '',
+    aptTime: '',
+    aptNotes: ''
+  }
+  let [toggleForm, setToggleForm] = useState(false)
+  let [formData, setFormData] = useState(clearData)
+
+  function formDataPublish() {
+    const appointmentInfo = {
+      id: lastId + 1,
+      ownerName: formData.ownerName,
+      petName: formData.petName,
+      aptDate: formData.aptDate + ' ' + formData.aptTime,
+      aptNotes: formData.aptNotes
+    }
+    onSendAppointment(appointmentInfo);
+    setFormData(clearData);
+    setToggleForm(!toggleForm)
   }
 
-  handleAdd(e) {
-    e.preventDefault();
-    let tempApt = {
-      petName: this.state.petName,
-      ownerName: this.state.ownerName,
-      aptDate: this.state.aptDate + ' ' + this.state.aptTime,
-      aptNotes: this.state.aptNotes
-    };
+  return (
+    <div>
+      <button onClick={() => { setToggleForm(!toggleForm) }}
+        className={`bg-blue-400 text-white px-2 py-3 w-full text-left rounded-t-md
+        ${toggleForm ? 'rounded-t-md' : 'rounded-md'}`}>
+        <div><BiCalendarPlus className="inline-block align-text-top" />  Add Appointment</div>
+      </button>
+      {
+        toggleForm &&
+        <div className="border-r-2 border-b-2 border-l-2 border-light-blue-500 rounded-b-md pl-4 pr-4 pb-4">
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label htmlFor="ownerName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              Owner Name
+          </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input type="text" name="ownerName" id="ownerName"
+                onChange={(event) => { setFormData({ ...formData, ownerName: event.target.value }) }}
+                value={formData.ownerName}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+            </div>
+          </div>
 
-    this.props.addAppointment(tempApt);
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label htmlFor="petName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              Pet Name
+          </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input type="text" name="petName" id="petName"
+                onChange={(event) => { setFormData({ ...formData, petName: event.target.value }) }}
+                value={formData.petName}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+            </div>
+          </div>
 
-    this.setState({
-      petName: '',
-      ownerName: '',
-      aptDate: '',
-      aptTime: '',
-      aptNotes: ''
-    });
-    this.props.toggleForm();
-  }
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label htmlFor="aptDate" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              Apt Date
+          </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input type="date" name="aptDate" id="aptDate"
+                onChange={(event) => { setFormData({ ...formData, aptDate: event.target.value }) }}
+                value={formData.aptDate}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+            </div>
+          </div>
 
-  handleChange(e) {
-    const target = e.target;
-    const value = target.value;
-    const name = target.name;
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label htmlFor="aptTime" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              Apt Time
+          </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input type="time" name="aptTime" id="aptTime"
+                onChange={(event) => { setFormData({ ...formData, aptTime: event.target.value }) }}
+                value={formData.aptTime}
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+            </div>
+          </div>
 
-    this.setState({
-      [name]: value
-    });
-  }
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label htmlFor="aptNotes" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+              Appointment Notes
+          </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <textarea id="aptNotes" name="aptNotes" rows="3"
+                onChange={(event) => { setFormData({ ...formData, aptNotes: event.target.value }) }}
+                value={formData.aptNotes}
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Detailed comments about the condition"></textarea>
+            </div>
+          </div>
 
-  render() {
-    return (
-      <div
-        className={
-          'card textcenter mt-3 ' +
-          (this.props.formDisplay ? '' : 'add-appointment')
-        }
-      >
-        <div
-          className="apt-addheading card-header bg-primary text-white"
-          onClick={this.props.toggleForm}
-        >
-          <FaPlus /> Add Appointment
+          <div className="pt-5">
+            <div className="flex justify-end">
+              <button type="submit" onClick={formDataPublish} className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400">
+                Submit
+            </button>
+            </div>
+          </div>
         </div>
-
-        <div className="card-body">
-          <form id="aptForm" noValidate onSubmit={this.handleAdd}>
-            <div className="form-group form-row">
-              <label
-                className="col-md-2 col-form-label text-md-right"
-                htmlFor="petName"
-                readOnly
-              >
-                Pet Name
-              </label>
-              <div className="col-md-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="petName"
-                  placeholder="Pet's Name"
-                  value={this.state.petName}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-
-            <div className="form-group form-row">
-              <label
-                className="col-md-2 col-form-label text-md-right"
-                htmlFor="ownerName"
-              >
-                Pet Owner
-              </label>
-              <div className="col-md-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="ownerName"
-                  placeholder="Owner's Name"
-                  value={this.state.ownerName}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-
-            <div className="form-group form-row">
-              <label
-                className="col-md-2 col-form-label text-md-right"
-                htmlFor="aptDate"
-              >
-                Date
-              </label>
-              <div className="col-md-4">
-                <input
-                  type="date"
-                  className="form-control"
-                  name="aptDate"
-                  id="aptDate"
-                  value={this.state.aptDate}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <label
-                className="col-md-2 col-form-label text-md-right"
-                htmlFor="aptTime"
-              >
-                Time
-              </label>
-              <div className="col-md-4">
-                <input
-                  type="time"
-                  className="form-control"
-                  name="aptTime"
-                  id="aptTime"
-                  value={this.state.aptTime}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-
-            <div className="form-group form-row">
-              <label className="col-md-2 text-md-right" htmlFor="aptNotes">
-                Apt. Notes
-              </label>
-              <div className="col-md-10">
-                <textarea
-                  className="form-control"
-                  rows="4"
-                  cols="50"
-                  name="aptNotes"
-                  id="aptNotes"
-                  placeholder="Appointment Notes"
-                  value={this.state.aptNotes}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-
-            <div className="form-group form-row mb-0">
-              <div className="offset-md-2 col-md-10">
-                <button
-                  type="submit"
-                  className="btn btn-primary d-block ml-auto"
-                >
-                  Add Appointment
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
+      }
+    </div>
+  )
 }
 
-export default AddAppointments;
+export default AddAppointment
